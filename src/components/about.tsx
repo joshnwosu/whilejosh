68e956f4f7747afb4d573a1354c6a4f45e719c52
joshnwosu <joshuanwosu078@gmail.com>
2023-01-28 14:37:13 +0100

    update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,21 @@
 import React from "react";
+import styles from "@/styles/Home.module.css";
 import Me from "./me";
 import Nav from "./nav";
+import { IoLogoTwitter, IoLogoGithub, IoIosRedo } from "react-icons/io";
+
+const DATA = [
+  {
+    title: "Twitter",
+    href: "",
+    icon: <IoLogoTwitter size={20} />,
+  },
+  {
+    title: "Github",
+    href: "",
+    icon: <IoLogoGithub size={20} />,
+  },
+];
 
 export default function About() {
   return (
@@ -8,6 +23,21 @@ export default function About() {
       <div>
         <Me />
         <Nav />
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.profile_pic}></div>
+        <ul className={styles.socials}>
+          {DATA.map(({ title, icon, href }, index) => (
+            <li key={index} className={styles.socials_li}>
+              <a href={href} className={styles.socials_link}>
+                {icon}
+                <span className={styles.socials_title}>{title}</span>
+                <IoIosRedo />
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import Nav from "@/components/nav";
-import Me from "@/components/me";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Blog from "@/components/blog";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -19,17 +13,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <section className={styles.section}>
+      <div className={styles.flex}>
+        <div className={styles.sticky}>
           <About />
-        </section>
-        <section className={styles.section}>
+        </div>
+        <div className={styles.scrollable}>
           <Projects />
           <Blog />
-        </section>
+        </div>
         <div className={styles.blur}></div>
         <div className={styles.blur}></div>
-      </main>
+      </div>
     </>
   );
 }
