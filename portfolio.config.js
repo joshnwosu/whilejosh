51a61a2f8f3a47abd8d068e682e1b8bd52605b95
jerry <jerrynwosu007@gmail.com>
2023-01-31 00:25:32 +0100

    update: add portfolio config data

---
+++ portfolio.config.js
@@ -0,0 +1,13 @@
+export const data = {
+  author: 'Jerry Nwosu',
+  socials: [
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/geekreflex',
+    },
+    {
+      name: 'Github',
+      url: 'https://github.com/geekreflex',
+    },
+  ],
+};
