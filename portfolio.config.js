a7a5d94c2ead73c3ad6c099252f7c7018116e862
jerry <jerrynwosu007@gmail.com>
2023-01-31 00:45:07 +0100

    update

--- portfolio.config.js
+++ portfolio.config.js
@@ -1,14 +1,16 @@
 export const data = {
-  author: 'Jerry Nwosu',
+  author: 'Joshua Nwosu',
   title: 'Software Engineer',
+  avatar:
+    'https://media.licdn.com/dms/image/C5603AQEEfwyQYyHXHw/profile-displayphoto-shrink_800_800/0/1613826760366?e=1680134400&v=beta&t=sltVISQB9nr2CCn4VSuxDsUItup-uaT7HRc-olvo7Tc',
   socials: [
     {
       name: 'Twitter',
-      url: 'https://twitter.com/geekreflex',
+      url: 'https://twitter.com/whilejosh',
     },
     {
       name: 'Github',
-      url: 'https://github.com/geekreflex',
+      url: 'https://github.com/whilejosh',
     },
   ],
 };
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -111,13 +111,7 @@ const About: React.FC = () => {
 
       <div className={styles.footer}>
         <div className={styles.profile_pic}>
-          <Image
-            src="https://media.licdn.com/dms/image/C5603AQEEfwyQYyHXHw/profile-displayphoto-shrink_800_800/0/1613826760366?e=1680134400&v=beta&t=sltVISQB9nr2CCn4VSuxDsUItup-uaT7HRc-olvo7Tc"
-            alt="My Image"
-            width={40}
-            height={40}
-          />
-          ;
+          <Image src={data.avatar} alt="My Image" width={40} height={40} />;
         </div>
         <ul className={styles.socials}>
           {DATA.map(({ title, icon, href }, index) => (
