a79ebcd8bc313ebc6bdf20fbb6778a91d0ddd49a
jerry <jerrynwosu007@gmail.com>
2023-01-31 03:02:52 +0100

    update: made with authors

--- src/components/madeWithLov.tsx
+++ src/components/madeWithLov.tsx
@@ -1,14 +1,31 @@
-import React from "react";
-import styles from "@/styles/Home.module.css";
+import React from 'react';
+import styles from '@/styles/Home.module.css';
 
 const MadeWithLove = () => {
   return (
     <div className={styles.madeWithLove}>
       <p className={styles.madeWithLove_para}>
-        Made with 🥤 by <a>Joshua Nwosu.</a>
+        Made with 🥤 by{' '}
+        <a
+          href="https://github.com/whilejosh"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.madeWithLove_link}
+        >
+          Joshua
+        </a>{' '}
+        &amps;{' '}
+        <a
+          href="https://github.com/geekreflex"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.madeWithLove_link}
+        >
+          Jerry
+        </a>
       </p>
       <p className={styles.madeWithLove_para}>
-        Props to{" "}
+        Props to{' '}
         <a
           href="https://github.com/sarahdayan/"
           target="_blank"
