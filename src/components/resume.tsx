b23f5196ba1c7fb80c5f5cb78b1a97f4207a1f16
joshnwosu <joshuanwosu078@gmail.com>
2023-01-31 01:51:54 +0100

    update

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -3,14 +3,21 @@ import styles from "@/styles/Home.module.css";
 import { IoIosClose } from "react-icons/io";
 import themeContext from "./themeContext";
 
-// import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const options = {
+  cMapUrl: "cmaps/",
+  cMapPacked: true,
+  standardFontDataUrl: "standard_fonts/",
+};
+// https://drive.google.com/file/d/1HylNDppAju6kj5Xhs4zZb9yk4hLSnuN1/view?usp=share_link
 
 export default function Resume() {
   const theme = React.useContext(themeContext);
 
-  //drive.google.com/file/d/1HylNDppAju6kj5Xhs4zZb9yk4hLSnuN1/view?usp=share_link
-
-  https: return (
+  return (
     <div
       className={[
         styles.resume,
@@ -24,6 +31,17 @@ export default function Resume() {
         }}
       >
         <IoIosClose size={30} color={theme.themeProps.textColor} />
+      </div>
+      <div className={styles.resume__wrapper}>
+        <Document
+          file={"Joshua_resume.pdf"}
+          options={options}
+          className={styles.resume__pdf}
+          renderMode="canvas"
+        >
+          <Page pageIndex={0} />
+        </Document>
+        {/* <iframe src="Joshua_resume.pdf"  /> */}
       </div>
     </div>
   );
