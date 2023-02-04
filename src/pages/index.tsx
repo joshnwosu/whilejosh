1756b7957e43fbc7bfa3e61255047b04b67dca89
jerry <jerrynwosu007@gmail.com>
2023-02-04 15:43:46 +0100

    add hello section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import About from "@/components/about";
-import Projects from "@/components/projects";
-import Blog from "@/components/blog";
-import React from "react";
-import themeContext from "@/components/themeContext";
-import Resume from "@/components/resume";
-import MadeWithLove from "@/components/madeWithLov";
-import { data } from "portfolio.config";
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+import About from '@/components/about';
+import Projects from '@/components/projects';
+import Blog from '@/components/blog';
+import React from 'react';
+import themeContext from '@/components/themeContext';
+import Resume from '@/components/resume';
+import MadeWithLove from '@/components/madeWithLov';
+import { data } from 'portfolio.config';
+import Hello from '@/components/hello';
 
 export default function Home() {
   const theme = React.useContext(themeContext);
@@ -22,15 +23,15 @@ export default function Home() {
 
       <style jsx global>{`
         :root {
-          --bg: ${theme.themeProps["bg"]};
-          --card: ${theme.themeProps["card"]};
-          --text-color: ${theme.themeProps["textColor"]};
-          --text-color-light: ${theme.themeProps["textColorLight"]};
-          --title: ${theme.themeProps["title"]};
-          --blur0: ${theme.themeProps["blur0"]};
-          --blur1: ${theme.themeProps["blur1"]};
-          --bgResume: ${theme.themeProps["bgResume"]};
-          --button: ${theme.themeProps["button"]};
+          --bg: ${theme.themeProps['bg']};
+          --card: ${theme.themeProps['card']};
+          --text-color: ${theme.themeProps['textColor']};
+          --text-color-light: ${theme.themeProps['textColorLight']};
+          --title: ${theme.themeProps['title']};
+          --blur0: ${theme.themeProps['blur0']};
+          --blur1: ${theme.themeProps['blur1']};
+          --bgResume: ${theme.themeProps['bgResume']};
+          --button: ${theme.themeProps['button']};
         }
       `}</style>
 
@@ -41,10 +42,11 @@ export default function Home() {
         <div className={styles.scrollable}>
           <Projects />
           <Blog />
+          <Hello />
           <MadeWithLove />
         </div>
-        <div className={[styles.blur, styles.blur0].join(" ")}></div>
-        <div className={[styles.blur, styles.blur1].join(" ")}></div>
+        <div className={[styles.blur, styles.blur0].join(' ')}></div>
+        <div className={[styles.blur, styles.blur1].join(' ')}></div>
         <Resume />
       </div>
     </>
